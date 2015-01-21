2828f7b13264070d96e06a8910aaeef9ec6d3609
Matt Ho <mattho@yahoo-inc.com>
2015-01-20 19:28:31 -0800

    Initial Commit

---
+++ RottenTomatos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RottenTomatos
+//
+//  Created by Matt Ho on 1/20/15.
+//  Copyright (c) 2015 Yahoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
