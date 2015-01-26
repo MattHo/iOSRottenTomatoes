edf6de69821a69a9d8a2c3b715d25a5a131396d2
Matt Ho <mattho@yahoo-inc.com>
2015-01-26 00:27:35 -0800

    Fine tune movie detail view

--- RottenTomatos/MovieDetailViewController.m
+++ RottenTomatos/MovieDetailViewController.m
@@ -11,6 +11,10 @@
 
 @interface MovieDetailViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsysLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 
 @end
 
@@ -19,11 +23,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self loadPoster];
+    [self setupScrollView];
+
+    self.title = self.movie[@"title"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)loadPoster {
     NSString *thumbnailUrl = [self.movie valueForKeyPath:@"posters.thumbnail"];
     NSString *originalUrl = [thumbnailUrl stringByReplacingOccurrencesOfString:@"_tmb" withString:@"_ori"];
-    
-    // self.titleLabel.text = self.movie[@"title"];
-    // self.synopsysLabel.text = self.movie[@"synopsis"];
+
     [self.posterView setImageWithURL:[NSURL URLWithString:thumbnailUrl]];
     [self.posterView setImageWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:originalUrl]] placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
         [UIView transitionWithView:self.posterView duration:2.0f options:UIViewAnimationOptionTransitionCrossDissolve animations:^{ self.posterView.image = image;
@@ -33,9 +47,23 @@
     }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) setupScrollView {
+    NSString *criticsScore = [[self.movie valueForKeyPath:@"ratings.critics_score"] stringValue];
+    NSString *audienceScore = [[self.movie valueForKeyPath:@"ratings.audience_score"] stringValue];
+    
+    [self.scrollView addSubview:self.titleLabel];
+    [self.scrollView addSubview:self.scoreLabel];
+    [self.scrollView addSubview:self.synopsysLabel];
+    
+    self.titleLabel.text = self.movie[@"title"];
+    self.scoreLabel.text = [NSString stringWithFormat:@"Critics Score:%@, Audience Score:%@\n%@", criticsScore, audienceScore, self.movie[@"mpaa_rating"]];
+    self.synopsysLabel.text = self.movie[@"synopsis"];
+    [self.synopsysLabel sizeToFit];
+
+    CGRect contentRect = CGRectZero;
+    for (UIView *view in self.scrollView.subviews)
+        contentRect = CGRectUnion(contentRect, view.frame);
+    self.scrollView.contentSize = contentRect.size;
 }
 
 /*
